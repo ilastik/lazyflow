fd7e0f615df609200359046e585db6898a3a7d6c
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2011-07-07 12:13:46 +0200

    further improvements of OpenGL painting

--- volumeeditor/imageScene2D.py
+++ volumeeditor/imageScene2D.py
@@ -54,12 +54,6 @@ class ImagePatch(object):
         return self.rect.width()
 
     def drawTexture(self):
-        #see 'backingstore' example by Ariya Hidayat
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        #this ensures a seamless transition between tiles
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         tx = self.rect.x()
         ty = self.rect.y()
         w = self.rect.width()
@@ -97,6 +91,7 @@ class ImageScene2D(QGraphicsScene):
         self._glWidget = glWidget
         self._useGL = (glWidget != None)
         self._shape2D = shape2D
+        self._updatableTiles = []
         
         self.imagePatches   = []
         patchAccessor = PatchAccessor(*self._shape2D, blockSize=self.blockSize)
@@ -105,8 +100,6 @@ class ImageScene2D(QGraphicsScene):
             r = patchAccessor.patchRectF(i)
             patch = ImagePatch(r)
             self.imagePatches.append(patch)
-            if self._useGL:
-                patch.texture = self._glWidget.bindTexture(patch.image)
         
         self.setSceneRect(0,0, *self._shape2D)
     
@@ -115,13 +108,10 @@ class ImageScene2D(QGraphicsScene):
 
     def updatePatch(self, patchNr):
         p = self.imagePatches[patchNr]
-        if p.dirty: return
+        self._updatableTiles.append(patchNr)
         
-        print "update patch %d" % (patchNr) 
-        
-        if self._useGL:
-            #self._glWidget.context().makeCurrent()
-            #p.texture = self._glWidget.bindTexture(p.image)
+        #print "update patch %d" % (patchNr)
+        if self._useGL:    
             QTimer.singleShot(20, self.update)
         else:
             self.invalidate(p.rectF, QGraphicsScene.BackgroundLayer)
@@ -139,35 +129,67 @@ class ImageScene2D(QGraphicsScene):
             patch.dirty = True
     
     def initializeGL(self):
-        print "xxxxxxxxxxxxxxxxxxxxx initializing GL"
         glDisable(GL_DEPTH_TEST)
         glEnable(GL_TEXTURE_2D)
+        glClearColor(0.0, 0.0, 0.0, 0.0);
+        glClear(GL_COLOR_BUFFER_BIT)
 
     def drawBackgroundGL(self, painter, rect):
-        #self._glWidget.context().makeCurrent()
-        #FIXME
+        #update the textures of those patches that were updated
+        for t in self._updatableTiles:
+            patch = self.imagePatches[t]
+            if patch.texture > -1:
+                self._glWidget.deleteTexture(patch.texture)
+            patch.texture = self._glWidget.bindTexture(patch.image)
+            #see 'backingstore' example by Ariya Hidayat
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+            #this ensures a seamless transition between tiles
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+        self._updatableTiles = []
         
         painter.beginNativePainting()
         #This will clear the screen, but also introduce flickering
-        #glClearColor(0.0, 1.0, 0.0, 1.0);
-        #glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.0, 1.0, 0.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
         
-##        glColor3f(0.0, 0.0, 1.0)
-##        glBegin(GL_QUADS)
-##        glVertex2f(x1, y1): glVertex2f(x2, y1): glVertex2f(x2, y2): glVertex2f(x1, y2)
-##        glEnd()
-
         drawnTiles = 0
         for i, patch in enumerate(self.imagePatches):
             if patch.dirty or not patch.rectF.intersect(rect): continue
             
-            #FIXME should not be necessary:
-            patch.texture = self._glWidget.bindTexture(patch.image)
-            
             patch.drawTexture()
             drawnTiles +=1
+            
+        def drawRect(x1, y1, w, h):
+            x1 = float(s.x()); y1 = float(s.y())
+            x2 = float(x1+w);  y2 = float(y1+h)
+            glRectd(x1, y1, x2, y2)
+        #  ************************************************
+        #  * rectangle s                                  *
+        #  *                                              *
+        #  *             ***************************      *                   
+        #  *             * rectangle r             *      *
+        #  *             ***************************      *                   
+        #  *                                              *                 
+        #  ************************************************
+         
+        #r = QRectF(0,0,*self._shape2D)
+        #s = rect
+        #print rect            
+        ##to clear the viewport, we would need sth. like this, crashes atm.
+        #glColor3f(0.0, 0.0, 1.0)
+        #if s.left() < r.left():
+        #    drawRect(s.x(), s.y(), r.left()-s.left(), s.height())
+        #if s.top() < r.top():
+        #    drawRect(r.x(), s.y(), r.width(), r.top()-s.top())
+        #if s.bottom()>r.bottom():
+        #    drawRect(r.left(), r.bottom(), r.width(), r.height())
+        #if s.right()<r.right():
+        #    drawRect(s.x(), s.y(), s.width()-r.width(), s.height())
+
         painter.endNativePainting()
-        print "ImageView2D.drawBackgroundGL: drew %d of %d tiles" % (drawnTiles, len(self.imagePatches))
+        #print "ImageView2D.drawBackgroundGL: drew %d of %d tiles" % (drawnTiles, len(self.imagePatches))
 
     def drawBackground(self, painter, rect):
         if self.useGL:
