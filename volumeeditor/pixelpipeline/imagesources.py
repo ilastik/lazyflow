0a7e123e487c279f76cfb9a0a44730b5a4635cda
Bernhard Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2011-07-20 11:24:31 +0200

    GrayscaleImageSource: implemented and tested

--- volumeeditor/pixelpipeline/imagesources.py
+++ volumeeditor/pixelpipeline/imagesources.py
@@ -1,45 +1,56 @@
 from PyQt4.QtCore import QObject, pyqtSignal
 from PyQt4.QtGui import QImage
 from qimage2ndarray import gray2qimage
+import asyncabcs
 
-class MonochromeImageSource( QObject ):
-    changed = pyqtSignal()
-    
-    @property
-    def rect( self ):
-        return self._rect
-    @rect.setter
-    def rect( self, rect ):
-        self._rect = rect
-        self.changed.emit()
+class GrayscaleImageRequest( object ):
+    def __init__( self, arrayrequest ):
+        self._arrayreq = arrayrequest
 
-    def __init__( self, sliceSource, rect = (0,0,128,128)):
-        super(MonochromeImageSource, self).__init__()
+    def wait( self ):
+        a = self._arrayreq.wait()
+        img = gray2qimage(a)
+        return img.convertToFormat(QImage.Format_ARGB32_Premultiplied)        
+    def notify( self, callback, **kwargs ):
+        self._arrayreq.notify(self._onNotify, package = (callback, kwargs))
+
+    def _onNotify( self, result, package ):
+        img = self.wait()
+        callback = package[0]
+        kwargs = package[1]
+        callback( img, **kwargs )
+asyncabcs.RequestABC.register(GrayscaleImageRequest)
+assert issubclass(GrayscaleImageRequest, asyncabcs.RequestABC)
+
+
+class GrayscaleImageSource( QObject ):
+    def __init__( self, sliceSource ):
+        assert isinstance(sliceSource, asyncabcs.ArraySourceABC)
+        super(GrayscaleImageSource, self).__init__()
         self._sliceSource = sliceSource
-        self._rect = rect
 
-    def request( self ):
-        s = self._sliceSource.request()
-        s = s[self.rect[0]: self.rect[0] + self.rect[2], self.rect[1]:self.rect[1] + self.rect[3]]
-        img = gray2qimage(s)
-        return img.convertToFormat(QImage.Format_ARGB32_Premultiplied)
+    def request( self, rect ):
+        req = self._sliceSource.request((slice(rect[0], rect[0] + rect[2]), slice(rect[1], rect[1] + rect[3])))
+        return GrayscaleImageRequest( req )
+asyncabcs.ImageSourceABC.register(GrayscaleImageSource)
+assert issubclass(GrayscaleImageSource, asyncabcs.ImageSourceABC)
 
-'''
-class AlphaModulatedImageSource( QObject ):
-    changed = pyqtSignal()
-    
-    @property
-    def rect( self ):
-        return self._rect
-    @rect.setter
-    def rect( self, rect ):
-        self._rect = rect
 
-    def __init__( self, sliceSource, rect = (0,0,256,128)):
-        super(ImageSource, self).__init__()
-        self.sliceSource = sliceSource
-        self._rect = rect
 
-    def request( self ):
-        pass
-'''
+import unittest as ut
+class GrayscaleImageSourceTest( ut.TestCase ):
+    def testRequest( self ):
+        from scipy.misc import lena
+        from datasources import ArraySource
+        raw = lena()
+        a = ArraySource(raw)
+        ims = GrayscaleImageSource( a )
+        imr = ims.request((0,0,512,512))
+
+        def check(result, codon):
+            self.assertEqual(codon, "unique")
+            self.assertTrue(type(result) == QImage)
+        imr.notify(check, codon="unique")
+
+if __name__ == '__main__':
+    ut.main()
