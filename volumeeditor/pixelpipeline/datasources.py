20fce86c372a8346fa93a23a46355e3ae4950b59
Bernhard Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2011-07-21 11:11:00 +0200

    datasource now only accept pure slicing. Added test for lazyflow source. Updated ArraySource test.

--- volumeeditor/pixelpipeline/datasources.py
+++ volumeeditor/pixelpipeline/datasources.py
@@ -1,6 +1,22 @@
 from PyQt4.QtCore import QObject, pyqtSignal
 from asyncabcs import RequestABC, ArraySourceABC
 
+def is_pure_slicing( slicing ):
+    '''Test if slicing is a single slice or sequence of slices.
+
+    Impure slicings may additionally contain integer indices, 
+    ellipses, booleans, or newaxis.
+    '''
+    if isinstance(slicing, slice):
+        return True
+    if not hasattr(slicing, '__iter__'):
+        return False
+    for thing in slicing:
+        if not isinstance(thing, slice):
+            return False
+    return True
+
+
 class ArrayRequest( object ):
     def __init__( self, result ):
         self._result = result
@@ -19,6 +35,8 @@ class ArraySource( QObject ):
         self._array = array
 
     def request( self, slicing ):
+        if not is_pure_slicing(slicing):
+            raise Exception('ArraySource: slicing is not pure')
         return ArrayRequest(self._array[slicing])
 ArraySourceABC.register( ArraySource )
 assert issubclass(ArraySource, ArraySourceABC)
@@ -44,8 +62,10 @@ class LazyflowSource( QObject ):
         self._outslot = outslot
 
     def request( self, slicing ):
+        if not is_pure_slicing(slicing):
+            raise Exception('ArraySource: slicing is not pure')
         reqobj = self._op.outputs[self._outslot][slicing].allocate()        
-        return Request( reqobject )
+        return LazyflowRequest( reqobj )
 ArraySourceABC.register( LazyflowSource )
 assert issubclass(LazyflowSource, ArraySourceABC)
 
@@ -62,25 +82,53 @@ class ArraySourceTest( ut.TestCase ):
         from scipy.misc import lena
         self.lena = lena()
 
-        raw = np.zeros((1,512,512,1,1))
-        raw[0,:,:,0,0] = self.lena
-        self.source = ArraySource( raw )
+        self.raw = np.zeros((1,512,512,1,1))
+        self.raw[0,:,:,0,0] = self.lena
+        self.source = ArraySource( self.raw )
 
     def testRequestWait( self ):
-        slicing = (0,slice(10,20), slice(20,25), 0, 0)
+        slicing = (slice(0,1),slice(10,20), slice(20,25), slice(0,1), slice(0,1))
         requested = self.source.request(slicing).wait()
-        self.assertTrue(self.np.all(requested == self.lena[10:20, 20:25]))
+        self.assertTrue(self.np.all(requested == self.raw[0:1,10:20,20:25,0:1,0:1]))
 
     def testRequestNotify( self ):
-        slicing = (0,slice(10,20), slice(20,25), 0, 0)
+        slicing = (slice(0,1),slice(10,20), slice(20,25), slice(0,1), slice(0,1))
         request = self.source.request(slicing)
         
         def check(result, codon):
-            self.assertTrue(self.np.all(result == self.lena[10:20, 20:25]))
+            self.assertTrue(self.np.all(result == self.raw[0:1,10:20,20:25,0:1,0:1]))
             self.assertEqual(codon, "unique")
         request.notify(check, codon="unique")
 
 
+
+try:
+    import lazyflow
+    has_lazyflow = True
+except ImportError:
+    has_lazyflow = False
+
+if has_lazyflow:
+    from lazyflow.graph import Graph
+    from _testing import OpDataProvider
+
+    class LazyflowSourceTest( ut.TestCase ):
+        def setUp( self ):
+            import numpy as np
+            self.np = np
+            from scipy.misc import lena
+            self.lena = lena()
+            self.raw = np.zeros((1,512,512,1,1), dtype=np.uint8)
+            self.raw[0,:,:,0,0] = self.lena
+
+            g = Graph()
+            op = OpDataProvider(g, self.raw)
+            self.source = LazyflowSource(op, "Data")
+
+        def testRequestWait( self ):
+            slicing = (slice(0,1), slice(10,20), slice(20,25), slice(0,1), slice(0,1))
+            requested = self.source.request(slicing).wait()
+            self.assertTrue(self.np.all(requested == self.raw[0:1,10:20,20:25,0:1,0:1]))
 
 
 
