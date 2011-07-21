c9295cc80546ecd19f82b27a4115f6ec991630d8
Bernhard Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2011-07-21 13:35:40 +0200

    added testing module for pixelpipeline

---
+++ volumeeditor/pixelpipeline/_testing.py
@@ -0,0 +1,42 @@
+try:
+    import lazyflow
+    has_lazyflow = True
+except ImportError:
+    has_lazyflow = False
+
+if has_lazyflow:
+    from lazyflow.graph import Graph, Operator, InputSlot, OutputSlot
+    from lazyflow import operators
+
+    class OpDelay(operators.OpArrayPiper):
+        def __init__( self, g, delay_factor = 0.000001 ):
+            super(OpDelay, self).__init__(g)
+            self._delay_factor = delay_factor
+
+        def getOutSlot(self, slot, key, resultArea):
+            req = self.inputs["Input"][key].writeInto(resultArea)
+            req.wait()
+            t = self._delay_factor*resultArea.nbytes
+            print "Delay: " + str(t) + " secs."
+            time.sleep(t)    
+
+    class OpDataProvider(Operator):
+        name = "Data Provider"
+        category = "Input"
+
+        inputSlots = [InputSlot("Changedata")]
+        outputSlots = [OutputSlot("Data")]
+
+        def __init__(self, g, data):
+            Operator.__init__(self,g)
+            self._data = data
+            oslot = self.outputs["Data"]
+            oslot._shape = self._data.shape
+            oslot._dtype = self._data.dtype
+
+        def getOutSlot(self, slot, key, result):
+            result[:] = self._data[key]
+
+        def setInSlot(self, slot, key, value):
+            self._data[key] = value
+            self.outputs["Output"].setDirty(key)
