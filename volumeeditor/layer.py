f91f4b76a30f4bcca4b82ca64fc55260290d224d
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2011-08-02 19:32:48 +0200

    Added signals to Layer

--- volumeeditor/layer.py
+++ volumeeditor/layer.py
@@ -1,4 +1,6 @@
-class Layer( object ):
+from PyQt4.QtCore import QObject, pyqtSignal
+
+class Layer( QObject ):
     '''Represents the visual properties and the associated raw data of a n-dimensional layer.
 
     properties:
@@ -7,14 +9,33 @@ class Layer( object ):
     opacity -- float; range 0.0 - 1.0
 
     '''
+    visibleChanged = pyqtSignal(bool)
+    opacityChanged = pyqtSignal(float)
+
+    @property
+    def visible( self ):
+        return self._visible
+    @visible.setter
+    def visible( self, value ):
+        self._visible = value
+        self.visibleChanged.emit( value )
+
+    @property
+    def opacity( self ):
+        return self._opacity
+    @opacity.setter
+    def opacity( self, value ):
+        self._opacity = value
+        self.opacityChanged.emit( value )
+
 
     @property
     def datasources( self ):
         return self._datasources
 
-    def __init__( self ):
-        self.visible = True
-        self.opacity = 1.0
+    def __init__( self, opacity = 1.0, visible = True ):
+        self._visible = visible
+        self._opacity = opacity
 
         self._datasources = []
 
