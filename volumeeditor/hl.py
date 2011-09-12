cab73582ed8fd3dcce1477f10a7c603ec12d0f9e
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2011-09-12 16:49:29 +0200

    added a highlevel entry point to the lib: hl.py

---
+++ volumeeditor/hl.py
@@ -0,0 +1,7 @@
+"""High-level API.
+
+"""
+from volumeeditor.pixelpipeline.datasources import *
+from volumeeditor.layer import *
+from volumeeditor.layerstack import LayerStackModel
+from volumeeditor.volumeEditor import VolumeEditor
